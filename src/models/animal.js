fcdd4ea43bc394e82878b75ef180ab71c0d9c8f9
Milen Herrera <milendayana@gmail.com>
2025-03-11 20:14:00 -0500

    Subiendo cambios

---
+++ src/models/animal.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const { stringify } = require("querystring");
+const animalSchema = mongoose.Schema({
+nombre: {
+    type: String,
+    required: true,
+},
+edad: {
+    type: Number,
+    required: true,
+},
+tipo:{
+    type: String,
+    required: true,
+},
+fecha:{
+    type: Date,
+    required: true,
+}
+});
+module.exports = mongoose.model("Animal", animalSchema);
